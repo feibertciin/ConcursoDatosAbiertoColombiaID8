ae8d2f5e0485a21544918292498bcc4499fad253
FEIBERT <ciin.com.co@gmail.com>
2026-07-17 15:19:53 -0500

    Delete Software directory

--- Software/core_app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import CSVUpload
-
-@admin.register(CSVUpload)
-class CSVUploadAdmin(admin.ModelAdmin):
-    list_display = ('title', 'total_records', 'predicted_dropouts', 'accuracy_estimate', 'uploaded_at')
-    list_filter = ('uploaded_at', 'accuracy_estimate')
-    search_fields = ('title',)
-    readonly_fields = ('total_records', 'predicted_dropouts', 'uploaded_at', 'pdf_report', 'notebook_file')
